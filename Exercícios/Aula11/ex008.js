dd15e532648278e6c4cda486246623e25a63760c
Ivancf-Bezerra <122057613+Ivancf-Bezerra@users.noreply.github.com>
2023-02-06 10:10:27 -0300

    aula 11 update

---
+++ Exercícios/Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida - MULTADO`)
+}
+console.log(`Dirija com segurança use cinto de segurança`) 
