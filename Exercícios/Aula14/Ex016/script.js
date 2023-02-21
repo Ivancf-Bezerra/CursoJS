68266fbe713d0ed88fd77d427fb07271d6fc03d2
Ivancf-Bezerra <122057613+Ivancf-Bezerra@users.noreply.github.com>
2023-02-21 08:03:33 -0300

    att

--- Exercícios/Aula14/Ex016/script.js
+++ Exercícios/Aula14/Ex016/script.js
@@ -1,13 +1,32 @@
-function contador(){
-var ini = document.getElementById('ini')
-var fim = document.getElementById('fim')
-var pass = document.getElementById('pass')
-var c = 1
-var res = document.getElementById('res')
-if (ini.value <= 0) {
-    window.alert('Inicio invalido')
-}
-    while (c <= fim.value){
-    res.innerHTML = `<p>${c.value} emojiaqui</p>` 
-}
-}
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossível contar'
+    } else {
+        res.innerHTML = 'Contando: ' 
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo inválido! Considerando PASSO ')
+            p = 1
+        }
+        
+        if(i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        } else{
+            for(let c= i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1f449}`
+
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
+} 
